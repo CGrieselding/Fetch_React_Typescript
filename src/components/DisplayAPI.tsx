e09a94fd7a23d0d005a933de2340b1254ed897e8
CGrieselding <camerongrieselding@gmail.com>
2021-09-17 14:06:17 -0400

    Addition of component files and assets folder

---
+++ src/components/DisplayAPI.tsx
@@ -0,0 +1,21 @@
+// Functional Component
+import React from "react";
+
+const DisplayAPI = (props: any) => {
+  const { temp, desc, feel, low, high, humid, wind } = props;
+
+  return (
+    <div>
+      <h2 className="mainTitleTwo"> Today's Weather! </h2>
+      <h4>Description: {desc}</h4>
+      <h4>Current Temperature: {Math.round((temp - 273.15) * 1.8 + 32)}°F</h4>
+      <h4>Real Feel: {Math.round((feel - 273.15) * 1.8 + 32)}°F</h4>
+      <h4>Low Temperature: {Math.round((low - 273.15) * 1.8 + 32)}°F</h4>
+      <h4>High Temperature: {Math.round((high - 273.15) * 1.8 + 32)}°F</h4>
+      <h4>Humidity: {humid}%</h4>
+      <h4>Wind Speed: {wind} m/s</h4>
+    </div>
+  );
+};
+
+export default DisplayAPI;
---
+++ src/components/GeoWeatherAPI.tsx
@@ -0,0 +1,87 @@
+// Class Component
+import React from "react";
+import DisplayAPI from "./DisplayAPI";
+import { render } from "react-dom";
+
+type StateType = {
+  latitude: number;
+  longitude: number;
+  desc: string[];
+  temp: string[];
+  feel: string[];
+  low: string[];
+  high: string[];
+  humid: string[];
+  wind: string[];
+};
+
+export default class GeoWeatherAPI extends React.Component<{}, StateType> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      desc: [],
+      temp: [],
+      feel: [],
+      low: [],
+      high: [],
+      humid: [],
+      wind: [],
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (myPosition) => {
+        console.log(myPosition);
+        this.setState({ latitude: myPosition.coords.latitude });
+        this.setState({ longitude: myPosition.coords.longitude });
+      },
+      (err) => {
+        console.error("Error code: " + err.code + " - " + err.message);
+      }
+    );
+    this.myWeather();
+  }
+
+  myWeather = () => {
+    const key = "e51267c357017ca3a21eada5bfb90157";
+    const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${key}`;
+
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.list[0]);
+        this.setState({ desc: data.list[0].weather[0].description });
+        this.setState({ temp: data.list[0].main.temp });
+        this.setState({ feel: data.list[0].main.feels_like });
+        this.setState({ low: data.list[0].main.temp_min });
+        this.setState({ high: data.list[0].main.temp_max });
+        this.setState({ humid: data.list[0].main.humidity });
+        this.setState({ wind: data.list[0].wind.speed });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="mainTitle">Geolocation App</h1>
+        <div>
+          <p>Your Latitude: {this.state.latitude}</p>
+          <p>Your Longitude: {this.state.longitude} </p>
+        </div>
+        <DisplayAPI
+          desc={this.state.desc}
+          temp={this.state.temp}
+          feel={this.state.feel}
+          low={this.state.low}
+          high={this.state.high}
+          humid={this.state.humid}
+          wind={this.state.wind}
+        />
+      </div>
+    );
+  }
+}
